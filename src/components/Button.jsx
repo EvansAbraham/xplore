51e8df7a24f5dfb813208ff6b76466c61827a5e0
EvansAbraham <jea150702@outlook.com>
2025-04-27 20:54:39 +0530

    finished hero section

---
+++ src/components/Button.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+
+const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
+  return (
+    <button id={id} className={`group z-10 relative w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+      {leftIcon}
+      <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
+        <div>
+          {title}
+        </div>
+      </span>
+      {rightIcon}
+    </button>
+  )
+}
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  rightIcon: PropTypes.node,
+  leftIcon: PropTypes.node,
+  containerClass: PropTypes.string,
+};
+
+
+export default Button
