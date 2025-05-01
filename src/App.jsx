05e594bc5474622e779a1d67d06137ce96ad83aa
EvansAbraham <jea150702@outlook.com>
2025-05-01 20:36:13 +0530

    Implemented Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import About from "./components/About"
 import Hero from "./components/Hero"
+import NavBar from "./components/NavBar"
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar/>
       <Hero/>
-      <section className="z-0 min-h-screen bg-blue-500"/>
+      <About/>
     </main>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -51,7 +51,7 @@ const Hero = () => {
     useGSAP(()=>{
         gsap.set('#video-frame', {
             clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
-            borderRadius: '0, 0, 40% 10%'
+            borderRadius: '0, 0, 40% 20%'
         });
         gsap.from('#video-frame',{
             clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
